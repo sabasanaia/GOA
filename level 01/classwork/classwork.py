e314ac06ab44ed8840ff33fa4d71e2ce55fa8e76
sabasanaia <sabasaba052512@gmail.com>
2024-09-09 19:25:14 +0400

    GOA

---
+++ level 01/classwork/classwork.py
@@ -0,0 +1,27 @@
+name = "saba" 
+# name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "saba" არის ცვლადისთვის მნიშვნელობა
+surname = "sanaia"
+
+print(name)
+# # პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+# name = "saba" # ეს არის str(string) ტიპის ცვლადი
+age = 13 # ეს არის int(integer) მთელი რიცხვი
+height = 1.75 # ეს არის float ტიპის ცვლადი (ათწილადი)
+
+knows_programming = True # True ან False
+is_ugly = False # snakecase(უბრალო წერის სტილი სტანდარტული)
+
+# სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+
+# print(type(age)) # age გადაეცა type ფუნქციას, რომელმაც დააბრუნა მისი ტიპი
+# და დაბრუნებული ნებისმიერი სიტყვა დავპრინტეთ, რომელმაც გაგვაგებინა რომ
+# print(type(age)) - ის ტიპი არის int(integer) (მთელი რიცხვი)
+
+print(type(age)) 
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_programming))
