6cde9e252090c4d5c98959921e4db77f84edd3af
sabasanaia <sabasaba052512@gmail.com>
2024-09-20 20:52:41 +0400

    GOA

---
+++ level05/classwork/classwork.py
@@ -0,0 +1,9 @@
+age = int(input("put in your age please:"))
+name1 = input("put in your name please:")
+name2 = input("put in your surname please:")
+print(age)
+print(name1)
+print(name2)
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+age = int(input("please put in your age"))
+print(age % 2)
