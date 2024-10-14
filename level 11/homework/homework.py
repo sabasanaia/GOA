6e303f5571dc3a980d9a983baf77507af6d0ccc6
sabasanaia <sabasaba052512@gmail.com>
2024-10-14 15:47:30 +0400

    GOA

---
+++ level 11/homework/homework.py
@@ -0,0 +1,32 @@
+cola = False
+pepsi = True
+print(pepsi or cola)
+cat = True
+dog = False
+print(dog and cat)
+tutel = False
+turtle = True
+print(tutel or turtle)
+soccer_ball = True
+basket_Ball = False
+print(soccer_ball and basket_Ball)
+TennisBall = False
+Racket = True
+print(TennisBall or Racket)
+phone = True
+charger = False
+print(phone and charger)
+prime = False
+lunchables = True
+print(prime or lunchables)
+question = True
+answer = False
+print(answer and question)
+#----------------------------------------------------------------------------------------------------------------------------------------------
+age = int(input("please enter your age "))
+name = input("please enter your name ")
+hight = float(input("please enter your hight"))
+print(age >= 18)
+print(name  >= "saba" )
+print(hight >= 1.80)
+#-----------------------------------------------------------------------------------------------------------------------------------------------
