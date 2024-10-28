0c8bea8188359d8e71750a23d003bb5f7b4049f8
sabasanaia <sabasaba052512@gmail.com>
2024-10-28 20:41:19 +0400

    GOA

---
+++ level 15/classwork/classwork.py
@@ -0,0 +1,6 @@
+password = "austrian painter"
+passq = input("ples enter your pessword ")
+while password != passq:
+    print("not correct")
+    passq = input("ples enter your pessword ")
+print("welcomeeee")
