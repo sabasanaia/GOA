1329fccf1708444571192b715ef503fd2a9e602c
sabasanaia <sabasaba052512@gmail.com>
2024-11-03 21:01:29 +0400

    GOA

---
+++ level 17/classwork/classwork.py
@@ -0,0 +1,5 @@
+name = ["giorgi","aleqsi","vano","saba","elene","qimera","vato"]
+for i in name:
+    if i == "giorgi":
+        print(i)
+
---
+++ level 17/homework/homework.py
@@ -0,0 +1,87 @@
+#1 
+name = [1,2,3,4,5]
+for i in name:
+    if i == 1:
+        print(i)
+for i in name:
+    if i == 2:
+        print(i)
+for i in name:
+    if i == 3:
+        print(i)
+for i in name:
+    if i == 4:
+        print(i)
+for i in name:
+    if i == 5:
+        print(i)
+#]------------------------------------------------------------------------------------------------------------------------------
+#2
+name = [1,2,3,4,5,6,7]
+for i in name:
+    if i == 1:
+        print(i)
+for i in name:
+    if i == 2:
+        print(i)
+for i in name:
+    if i == 3:
+        print(i)
+for i in name:
+    if i == 4:
+        print(i)
+for i in name:
+    if i == 5:
+        print(i)
+    if i == 6:
+        print(i)
+for i in name:
+    if i == 7:
+        print(i)
+for i in name:
+    if i == 8:
+        print(i)
+for i in name:
+    if i == 9:
+        print(i)
+for i in name:
+    if i == 10:
+        print(i)
+#==================================================================================================================================
+#3
+name = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+for i in name:
+    if i > 10:
+        print(i)
+#~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~~-~-~
+#4
+name = ["1",2,3,"4",5,6.0,"7"]
+for i in name:
+    if i == "1":
+        print(i)
+        print(type(i))
+for i in name:
+    if i == 2:
+        print(i)
+        print(type(i))
+for i in name:
+    if i == 3:
+        print(i)
+        print(type(i))
+for i in name:
+    if i == "4":
+        print(i)
+        print(type(i))
+for i in name:
+    if i == 5:
+        print(i)
+        print(type(i))
+for i in name:
+    if i == 6.0:
+        print(i)
+        print(type(i))
+for i in name:
+    if i == "7":
+        print(i)
+        print(type(i))
+
