1ed3c482d118510ca2623760260d8cb47dc54ea7
sabasanaia <sabasaba052512@gmail.com>
2025-01-09 21:01:26 +0400

    GOA

--- level 35/classwork/classwok.py
+++ level 35/classwork/classwok.py
@@ -4,3 +4,53 @@ def capitals(word):
         if word[i].isupper():#ვამოწმებთ თუ ინდექსის ელემენტი დიდი ასოთია დაწერილი
             list.append(i)#ყველა ელემენტი რომელიც დიდ ასოთი არის დწერილი ემატება ლისტს
     return list#ვაბრუნებთ ლისტს
+
+def max_multiple(divisor, bound):
+    for i in range(bound,1,-1):
+        if i % divisor == 0:
+            return i
+            
+def sum_digits(number):
+    sum = 0
+    if number < 0:
+        number *= -1
+    for i in str(number):
+        sum += int(i)
+    return sum
+
+def dont_give_me_five(start,end):
+    result = []
+    for i in range(start,end + 1):
+        if "5" not in str(i):
+            result.append(i)
+    return len(result)
+
+def fizzbuzz(n):
+    list = []
+    for i in range(1,n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            list.append("FizzBuzz")
+        elif i % 3 == 0:
+            list.append("Fizz")
+        elif i % 5 == 0:
+            list.append("Buzz")
+        else:
+            list.append(i)
+    return list
+
+def invert(lst):
+    list = []
+    for i in lst:
+        i *= -1
+        list.append(i)
+    return list
+
+def sum_array(arr):
+    sum = 0
+    if arr == None:
+        return 0 
+    if len(arr) <= 2:
+        return 0
+    for i in arr:
+        sum += i
+    return sum - max(arr) - min(arr)
---
+++ level 35/homework/homework.py
@@ -0,0 +1,48 @@
+def to_jaden_case(string):
+    list = string.split(" ")
+    result = []
+    for string in list:
+        result.append(string.capitalize())
+    return " ".join(result)
+    
+        
+def sequence_sum(begin_number, end_number, step):
+    sum = 0
+    for i in range(begin_number,end_number + 1,step):
+        sum += i
+    return sum
+
+def lowercase_count(string):
+    count = 0
+    lower_letters = "qwertyuiopasdfghjklzxcvbnm"
+    for i in string:
+        if i in lower_letters:
+            count += 1
+    return count
+
+def better_than_average(class_points, your_points):
+    sum = 0
+    for i in range(len(class_points)):
+        sum += class_points[i]
+    average = sum / len(class_points)
+    return average < your_points
+
+def grow(arr):
+    multiply = 1
+    for i in arr:
+        multiply *= i
+    return multiply
+
+def smash(words):
+    return " ".join(words)
+
+#---------------------------------------------------------------- manual join------------------------------------------------------------------------
+def manual_join(lst,separator):
+    result = ""
+    for i in range(len(lst)):
+        if i != len(lst) - 1:
+            result += lst[i] + separator
+        else:
+            result += lst[i]
+    return result
+print(manual_join(["hello","i","am","12","years","old"]," "))
