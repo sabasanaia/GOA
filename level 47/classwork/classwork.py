9f013090d0d9611a26b314f3100feb691cb835bc
sabasanaia <sabasaba052512@gmail.com>
2025-02-22 22:19:29 +0400

    GOA

---
+++ level 47/classwork/classwork.py
@@ -0,0 +1,51 @@
+def expression_matter(a, b, c):
+    first = a * (b + c)
+    second = a * b * c
+    third = a + b * c
+    forth = (a + b) * c
+    fifth = a + b + c
+    return max(first,second,third,forth,fifth)
+
+def dup(arry):
+    result = []
+    for word in arry:
+        new_word = ""
+        for i in range(len(word)):
+            if i == 0 or word[i] != word[i - 1]:
+                new_word += word[i]
+        result.append(new_word)
+    return result
+
+def spin_words(sentence):
+    result = []
+    for i in sentence.split(" "):
+        if len(i) >= 5:
+            result.append(i[::-1])
+        else:
+            result.append(i)
+    return " ".join(result)
+
+def min_value(digits):
+    result = []
+    for i in sorted(set(digits)):
+        result.append(str(i))
+    return int("".join(result))
+
+def max_multiple(divisor, bound):
+    for i in range(bound,1,-1):
+        if i % divisor == 0:
+            return i
+
+def alphabetic(s):
+    return s == "".join(sorted(s))
+
+def case_sensitive(s):
+    result = []
+    if s.islower() == True or s == "":
+        return [True, []]
+    if s.islower() == False:
+        big_letters = []
+        for i in s:
+            if i.isupper() == True:
+                big_letters.append(i)
+        return  [False, big_letters]
---
+++ level 47/homework/homework.py
@@ -0,0 +1,57 @@
+# 8 kyu
+def whatday(num):
+    if num == 1:
+        return "Sunday"
+    elif num == 2:
+        return "Monday"
+    elif num == 3:
+        return "Tuesday"
+    elif num == 4:
+        return "Wednesday"
+    elif num == 5:
+        return "Thursday"
+    elif num == 6:
+        return "Friday"
+    elif num == 7:
+        return "Saturday"
+    else:
+        return "Wrong, please enter a number between 1 and 7"
+    
+def rain_amount(mm):
+    if mm >= 40:
+         return "Your plant has had more than enough water for today!"
+    elif mm < 40:
+         return f"You need to give your plant {40 - mm}mm of water"
+
+def lovefunc( flower1, flower2 ):
+    if flower1 % 2 != 0 and flower2 % 2 == 0:
+        return True
+    elif flower2 % 2 != 0 and flower1 % 2 == 0:
+        return True
+    else:
+        return False
+    
+# 7kyu
+def high_and_low(numbers):
+    numbers = numbers.split(" ")
+    for i in range(len(numbers)):
+        numbers[i] = int(numbers[i])
+        
+    return f"{max(numbers)} {min(numbers)}"
+
+def get_count(sentence):
+    result = []
+    for i in sentence:
+        if i == "a":
+            result.append(i)
+        elif i == "e":
+            result.append(i)
+        elif i == "i":
+            result.append(i)
+        elif i == "o":
+            result.append(i)
+        elif i == "u":
+            result.append(i)
+    return len(result)
+
+#6kyu
